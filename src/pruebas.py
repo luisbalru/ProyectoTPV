c0baa7e9916ee52cad4dd8f598dbb45311530815
Jose María Viñas Rodríguez <jmvinasr@correo.ugr.es>
2018-10-09 17:03:45 +0200

    Closes #5

---
+++ src/pruebas.py
@@ -0,0 +1,150 @@
+import json
+import time
+import os
+
+class TPV():
+    """Clase para el microservicio TPV"""
+
+    def __init__(self):
+        self.__gananciasDia = 0
+        try:
+            if os.path.exists('data/mesas.json'):
+                with open('data/mesas.json', 'r') as f:
+                    self.mesas = json.load(f)
+            else:
+                raise IOError("No se encuentra 'mesas.json'")
+
+            if os.path.exists('data/precios.json'):
+                with open('data/precios.json', 'r') as f:
+                    self.precios = json.load(f)
+            else:
+                raise IOError("No se encuentra 'precios.json'")
+
+            if os.path.exists('data/stock.json'):
+                with open('data/stock.json', 'r') as f:
+                    self.stock = json.load(f)
+            else:
+                raise IOError("No se encuentra 'stock.json'")
+
+        except IOError as fallo:
+             print("Error {} leyendo hitos.json".format( fallo ) )
+
+    def CuentasMesas(self):
+        return len(self.mesas)
+
+    #Reduce la cantidad de stock disponible
+    def ReducirStock(self,nMesa):
+        if len(self.mesas) > nMesa and nMesa >=0:
+            self.stock["bebidas"] -= self.mesas[nMesa]["bebidas"]
+            self.stock["raciones"] -= self.mesas[nMesa]["raciones"]
+            self.stock["pan"] -= self.mesas[nMesa]["pan"]
+            return True
+        else:
+            return False
+
+    #La cantidad de mesas no varia, o estan libres(todo a 0) o estan ocupadas
+    def VaciarMesa(self,nMesa):
+        if len(self.mesas) > nMesa and nMesa >=0:
+            self.mesas[nMesa]["bebidas"] = 0
+            self.mesas[nMesa]["raciones"] = 0
+            self.mesas[nMesa]["pan"] = 0
+            return True
+        else:
+            return False
+
+    #Añadir a la cuenta los nuevos pedidos
+    def Apuntar(self,nMesa,bebidas=None,raciones=None,pan=None):
+        if bebidas:
+            if (self.stock["bebidas"]-bebidas) < 0:
+                return False
+            else:
+                self.mesas[nMesa]["bebidas"] += bebidas
+        if raciones:
+            if (self.stock["raciones"]-raciones) < 0:
+                return False
+            else:
+                self.mesas[nMesa]["raciones"] += raciones
+        if pan:
+            if (self.stock["pan"]-pan) < 0:
+                return False
+            else:
+                self.mesas[nMesa]["pan"] += pan
+
+        return True
+
+    def ReponerStock(self,pan=0,raciones=0,bebidas=0):
+        #suponiendo que no se gastan todas, se añaden a las que quedan
+        if pan > 0 and raciones > 0 and bebidas > 0:
+            self.stock["bebidas"] += bebidas
+            self.stock["raciones"] += raciones
+            self.stock["pan"] += pan
+            return True
+        else:
+            return False
+
+    def Cuenta(self,nMesa):
+        if len(self.mesas) > nMesa and nMesa >=0:
+            return (self.mesas[nMesa]["bebidas"]*self.precios["bebidas"] +
+            self.mesas[nMesa]["pan"]*self.precios["pan"]+
+            self.mesas[nMesa]["raciones"]*self.precios["raciones"])
+        else:
+            return False
+
+    def mod11_cuenta_bancaria(self,numero):
+        if len(numero)!=10:
+            return False
+        cifras = [1,2,4,8,5,10,9,7,3,6]
+        chequeo = 0
+        for i in range(10):
+            chequeo += int(numero[i])*cifras[i]
+
+        chequeo = 11 - (chequeo%11)
+        if chequeo == 11:
+            chequeo = 0
+        if chequeo == 10:
+            chequeo = 1
+        return chequeo
+
+    def ValidarTarjeta(self,entidad,oficina,dc,cuenta):
+        if len(entidad) != 4:
+            return False
+        if len(oficina) != 4:
+            return False
+        if len(dc) != 2:
+            return False
+        if len(cuenta) != 10:
+            return False
+        cadena = "00"+entidad+oficina
+        print(cadena)
+        if self.mod11_cuenta_bancaria(cadena)!=int(dc[0]):
+            return False
+        if self.mod11_cuenta_bancaria(cuenta) !=int(dc[1]):
+            return False
+        return True
+
+    def Cobrar(self,numeroMesa,esTarjeta=False,entidad=None,oficina=None,dc=None,cuenta=None):
+        if len(self.mesas) > numeroMesa and numeroMesa >=0:
+            self.ReducirStock(numeroMesa)
+            total = self.Cuenta(numeroMesa)
+            self.VaciarMesa(numeroMesa)
+            if esTarjeta:
+                if self.ValidarTarjeta(entidad,oficina,dc,cuenta):
+                    self.__gananciasDia += total
+                    return True
+                else:
+                    print("Numero de tarjeta incorrecto")
+                    return False
+            else: #efectivo
+                self.__gananciasDia += total
+                return True
+        else:
+            return False
+
+    #devuelve las ganancias del dia y restaura el valor de las ganancias
+    def HacerCaja(self):
+        fecha = time.strftime("%d/%m/%Y")
+        with open('data/ganancias.json','r') as f:
+            ganancias = json.load(f)
+        ganancias.append({"fecha":fecha,"ganancias":self.__gananciasDia})
+        self.__gananciasDia = 0
+        return ganancias[len(ganancias)-1]["ganancias"]
